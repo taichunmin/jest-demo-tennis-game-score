920ef3a715d6985686d70ee3660b811e1bd968cb
taichunmin <taichunmin@gmail.com>
2019-04-29 10:53:56 +0800

    is win

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-const SCORE_STR = ['love', '15', '30', '40', 'game']
+const SCORE_STR = ['love', '15', '30', '40']
 
 exports.score = (pa, pb) => {
+  if (exports.isWin(pa, pb)) return 'A win.'
+  if (exports.isWin(pb, pa)) return 'B win.'
+
   if (exports.isSameScore(pa, pb)) return `${SCORE_STR[pa]}-all`
   return `${SCORE_STR[pa]}-${SCORE_STR[pb]}`
 }
+
+exports.isWin = (pa, pb) => pa >= 4 && pa >= pb + 2
 
 exports.isSameScore = (pa, pb) => pa === pb
--- index.test.js
+++ index.test.js
@@ -5,6 +5,8 @@ test.each([
   [1, 0, '15-love'],
   [3, 0, '40-love'],
   [0, 3, 'love-40'],
+  [4, 0, 'A win.'],
+  [0, 4, 'B win.'],
 ])('比數是 %d:%d 的時候應該顯示 %s', (pa, pb, expected) => {
   expect(TennisGameScore.score(pa, pb)).toEqual(expected)
 })
